151d10f9cd67270f7fcccaab2d4b09379f504c86
Isaac Dorfman <isaacdorfman@Isaacs-MacBook-Pro.local>
2019-09-11 18:53:16 -0400

    new pics

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ import pandas as pd
 
 ########### Define your variables ######
 
-tabtitle = 'the district'
-myheading1 = 'Step Back! The doors are about to close!'
-myheading2 = 'Back2Good'
-image1 = 'dc-flag.jpg'
-image2 = 'metro-map.png'
-textbody = "Metro's recent on time performance ratings are so good, it's getting harder to find a good reason to take a car!"
+tabtitle = 'RushHourPromise'
+myheading1 = 'Step Back! Oh God is that a fire!!!'
+myheading2 = 'Back2Good?'
+image1 = 'US_Capitol_west_side.jpeg'
+image2 = 'MetroFireThursday.jpg'
+textbody = "Metro's recent on time performance ratings are so good, but the author of this app still got a fare reimbursement recently!"
 sourceurl = 'https://www.wmata.com/about/back2good/index.cfm'
-githublink = 'https://github.com/austinlasseter/dash-dc-layout'
+githublink = 'https://github.com/ihdorfman/dash-dc-layout'
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -36,7 +36,7 @@ app.layout = html.Div(children=[
             html.Div(textbody, style={
                 'padding': '12px',
                 'font-size': '22px',
-                'height': '120px',
+                'height': '240px',
                 'border': 'thin red solid',
                 'color': 'rgb(255, 255, 255)',
                 'backgroundColor': 'rgb(57, 83, 107)',
